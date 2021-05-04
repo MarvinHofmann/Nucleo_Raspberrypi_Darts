6f44fab786a9cd6bba5b03bad6d609c8dd4a0913
MaHo <marvin@raithweg15.de>
2021-05-04 20:17:13 +0200

    erweiterung der checkIfWon Methode, sowie hinzufügen von Kommentaren

--- public/GameBoardLogik.js
+++ public/GameBoardLogik.js
@@ -1,16 +1,20 @@
-const ws = new WebSocket('ws://192.168.0.46:8080');
+const ws = new WebSocket('ws://192.268.0.46:8080');
 
 let averageZaehler = 0;
 let zaehler = 0;
 let zwischenSumme = 0;
 let dartRest = 501;
 
+let MasterHandler = true; // Wenn True darf gespielt werden wenn false Spiel vorbei
+
 ws.addEventListener('open', () => {
     console.log("Client connected with server!")
 });
 
 ws.addEventListener('message', function (event){
-    const data = JSON.parse(event.data);
+
+  if(MasterHandler){
+  const data = JSON.parse(event.data);
     
     zaehler++;
     
@@ -24,13 +28,16 @@ ws.addEventListener('message', function (event){
         break;
   
       case 'stringErgebnis': 
-        checkIfWon(data.value);
-        handleStringErgebnise(data.value);
+        if(!checkIfWon(data.value)){
+          handleStringErgebnise(data.value);
+        }
         break;
+        
 
     default:
         // Unknown websocket message type
-    }   
+    }     
+  }   
   });
 
   function handleStringErgebnise(data){
@@ -58,9 +65,10 @@ ws.addEventListener('message', function (event){
           break;
     
         case 'stringErgebnis': 
-          checkIfWon(data.value);
-          handleStringErgebnise(data.value);
-          break;
+          if(!checkIfWon(data.value)){
+            handleStringErgebnise(data.value);
+         }
+         break;
     }
     dartRest -= zwischenSumme;
     document.getElementById("restWert").innerText = dartRest;
@@ -81,12 +89,29 @@ ws.addEventListener('message', function (event){
   }
 
   function checkIfWon(uebergebenerString){
-    split = Array.from(uebergebenerString);
+    split = Array.from(uebergebenerString); //Split von String in Char Array zur Überprüfung auf Double 
     if(split[0] == 'D' && dartRest - zwischenSumme == 0){
+      //Wenn Aktueller Ergebnis 0 und der Pfeil in Doppel Feld ist das Spiel beendet
       console.log('Gewonnen');
-    }else{
-      dartRest+=zwischenSumme;
-      console.log('nicht gewonnen');
-    }
+      zwischenSumme=0;
+      averageZaehler++;
+      handleStringErgebnise(uebergebenerString);
+      document.getElementById("average").innerText = getAverage(averageZaehler);
+      dartRest=0;
+      document.getElementById("restWert").innerText = dartRest;
+      document.getElementById("status").innerHTML = 'Gewonnen';
+      MasterHandler = false;
+      return true;
 
+    }else if(dartRest - zwischenSumme <=0 || dartRest -zwischenSumme == 1){
+      //Wenn das Ergebnis kleiner null oder 1 ist dann ist nicht Gewonnen sondern überworfen
+      zwischenSumme = 0;
+      document.getElementById("ergWurf1").innerText = String('/');
+      document.getElementById("ergWurf2").innerText = String('/'); //Setzte felder zurück
+      document.getElementById("ergWurf3").innerText = String('/');
+      zaehler=0;
+      console.log('nicht gewonnen und Wert kleiner 0');
+      document.getElementById("status").innerHTML = 'Überworfen';
+      return false;
+    }
   }
